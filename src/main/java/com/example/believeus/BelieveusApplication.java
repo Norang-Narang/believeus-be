294bfb3f0f61dd96db181c5bdc3ca8f816acbe28
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-12 22:28:32 +0900

    Project Setting

---
+++ src/main/java/com/example/believeus/BelieveusApplication.java
@@ -0,0 +1,13 @@
+package com.example.believeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelieveusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelieveusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/believeus/BelieveusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.believeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelieveusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
