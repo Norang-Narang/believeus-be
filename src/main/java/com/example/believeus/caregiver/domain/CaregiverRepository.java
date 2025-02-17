996f39528cd657173977c3c99e5a8bfc15f9150e
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-17 14:07:57 +0900

    feat: CaregiverRepository 작성
    - ID 기반 조회 방식 사용
    - 추후 요양보호사를 식별할 다른 기준이 추가된다면 UNIQUE 속성 적용 필요

---
+++ src/main/java/com/example/believeus/caregiver/domain/CaregiverRepository.java
@@ -0,0 +1,8 @@
+package com.example.believeus.caregiver.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CaregiverRepository extends JpaRepository<Caregiver, Long> {
+    Optional<Caregiver> findById(Long id);
+}
