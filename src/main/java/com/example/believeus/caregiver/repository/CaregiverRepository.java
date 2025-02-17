e94e99242c17825592b42c0a71e1305d3c15fb35
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-17 15:03:27 +0900

    update: CaregiverRepository 패키지 경로 변경 (domain->repository)

--- src/main/java/com/example/believeus/caregiver/repository/CaregiverRepository.java
+++ src/main/java/com/example/believeus/caregiver/repository/CaregiverRepository.java
@@ -1,8 +1,8 @@
-package com.example.believeus.caregiver.domain;
+package com.example.believeus.caregiver.repository;
 
+import com.example.believeus.caregiver.domain.Caregiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CaregiverRepository extends JpaRepository<Caregiver, Long> {
-    Optional<Caregiver> findById(Long id);
 }
