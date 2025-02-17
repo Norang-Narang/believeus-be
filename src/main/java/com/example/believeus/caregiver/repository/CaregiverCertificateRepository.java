3fbdbb79bcd602e5598ba8381672ef8cb0fe271b
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-17 15:07:50 +0900

    feat: CaregiverCertificateRepository 추가
    - CaregiverCertificate는 요양보호사의 자격증 정보 저장용 테이블
    - 회원가입 시 자격증 정보를 함께 저장하려면 별도의 Repository가 필요
    - CaregiverCertificateRepository는 JPA를 활용해 DB에 접근하는 역할

---
+++ src/main/java/com/example/believeus/caregiver/repository/CaregiverCertificateRepository.java
@@ -0,0 +1,7 @@
+package com.example.believeus.caregiver.repository;
+
+import com.example.believeus.caregiver.domain.CaregiverCertificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaregiverCertificateRepository extends JpaRepository<CaregiverCertificate, Long> {
+}
