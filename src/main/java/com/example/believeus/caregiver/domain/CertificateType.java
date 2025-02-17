946ca9d56f55e66938d786b5649d203c59e83bc1
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-17 15:47:02 +0900

    fix: Enum에서 JSON 요청을 받을 때 displayName을 기반으로 Enum을 찾을 수 있도록 설정

--- src/main/java/com/example/believeus/caregiver/domain/CertificateType.java
+++ src/main/java/com/example/believeus/caregiver/domain/CertificateType.java
@@ -1,13 +1,17 @@
 package com.example.believeus.caregiver.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
+@Getter
 public enum CertificateType {
     CAREGIVER("요양보호사"),
-    SOCIAL_WORKER_L1("사회복지사 1급"),
-    SOCIAL_WORKER_L2("사회복지사 2급"),
-    NURSING_ASSISTANT_L1("간호조무사 1급"),
-    NURSING_ASSISTANT_L2("간호조무사 2급");
+    SOCIAL_WORKER_L1("사회복지사_1급"),
+    SOCIAL_WORKER_L2("사회복지사_2급"),
+    NURSING_ASSISTANT_L1("간호조무사_1급"),
+    NURSING_ASSISTANT_L2("간호조무사_2급");
 
     private final String displayName;
 
@@ -18,5 +22,15 @@ public enum CertificateType {
     @JsonValue
     public String getDisplayName() {
         return displayName;
+    }
+
+    @JsonCreator
+    public static CertificateType fromValue(@JsonProperty("certificateType") String value) {
+        for (CertificateType type : CertificateType.values()) {
+            if (type.getDisplayName().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid certificate type: " + value);
     }
 }
