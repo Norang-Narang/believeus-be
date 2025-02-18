05d33dede062b10695c7a7d28828191c9b14cb79
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-19 00:01:05 +0900

    fix: cicd 오류 수정
    - allowedOrigins -> allowedOriginPatterns 변경
    - SeniorRepository customize 메서드 기본 구현 추가

--- src/main/java/com/example/believeus/config/WebConfig.java
+++ src/main/java/com/example/believeus/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
                 .allowCredentials(true)
                 .maxAge(3600);
