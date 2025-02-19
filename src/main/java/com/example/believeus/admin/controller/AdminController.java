8e22fae79e8ee784c7c327d72e1a055d45a8de8b
hyeonjoonpark <pjjoon1379@gmail.com>
2025-02-19 21:22:33 +0900

    feat: 어르신 전체 정보 조회 API

--- src/main/java/com/example/believeus/admin/controller/AdminController.java
+++ src/main/java/com/example/believeus/admin/controller/AdminController.java
@@ -1,9 +1,14 @@
 package com.example.believeus.admin.controller;
 
 import com.example.believeus.admin.application.AdminService;
+import com.example.believeus.admin.dto.SeniorListResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Admin API", description = "관리자 API")
 @RestController
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
+
+    @Operation(summary = "전체 어르신 목록 조회", description = "전체 어르신의 정보를 가져옵니다")
+    @GetMapping("/seniors")
+    public ResponseEntity<List<SeniorListResponse>> getSeniors() {
+        return adminService.seniors();
+    }
 }
