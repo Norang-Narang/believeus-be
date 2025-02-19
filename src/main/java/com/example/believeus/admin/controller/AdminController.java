9e60de316b91ccb95fcac3bfc603247af7a35ec9
hyeonjoonpark <pjjoon1379@gmail.com>
2025-02-19 21:33:20 +0900

    feat: ADMIN만 접근 가능하도록 @PreAuthorize 어노테이션 추가

--- src/main/java/com/example/believeus/admin/controller/AdminController.java
+++ src/main/java/com/example/believeus/admin/controller/AdminController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class AdminController {
     private final AdminService adminService;
 
     @Operation(summary = "전체 어르신 목록 조회", description = "전체 어르신의 정보를 가져옵니다")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/seniors")
     public ResponseEntity<List<SeniorListResponse>> getSeniors() {
         return adminService.seniors();
